73c9386567d05f69acb988ec4b4ba2d6a9ffff56
Olena Lymar <lena.lumar2003@gmail.com>
2023-11-30 16:25:44 +0100

    first commit

---
+++ movieist/src/test/java/dev/olena/backend/MovieistApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olena.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
